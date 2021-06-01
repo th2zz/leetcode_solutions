68c66be2204a6346ecd1446b16e525eccbef7385
th2zz <tzhang329@gmail.com>
2021-06-01 23:22:03 +0800

    add files

---
+++ number_of_islands/solution.py
@@ -0,0 +1,35 @@
+class Solution:
+    """
+    Description
+给一个 01 矩阵，求不同的岛屿的个数。
+
+0 代表海，1 代表岛，如果两个 1 相邻，那么这两个 1 属于同一个岛。我们只考虑上下左右为相邻。
+
+Example
+样例 1：
+
+输入：
+[
+  [1,1,0,0,0],
+  [0,1,0,0,1],
+  [0,0,0,1,1],
+  [0,0,0,0,0],
+  [0,0,0,0,1]
+]
+输出：
+3
+样例 2：
+
+输入：
+[
+  [1,1]
+]
+输出：
+1
+
+    @param grid: a boolean 2D matrix
+    @return: an integer
+    """
+    def numIslands(self, grid):
+        # write your code here
+        pass
