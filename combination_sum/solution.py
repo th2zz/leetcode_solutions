a7a1ed42a21c6f11fa1e44b3d0b7bbcb0a322df4
th2zz <tzhang329@gmail.com>
2021-06-24 16:58:34 +0800

    combination sum, string permutation

---
+++ combination_sum/solution.py
@@ -0,0 +1,43 @@
+class Solution:
+    """https://www.lintcode.com/problem/135/?_from=collection&fromId=161
+    Description
+Given a set of candidate numbers candidates and a target number target. Find all unique combinations in candidates
+where the numbers sums to target.
+
+The same repeated number may be chosen from candidates unlimited number of times.
+
+All numbers (including target) will be positive integers.
+Numbers in a combination a1, a2, … , ak must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak)
+Different combinations can be in any order.
+The solution set must not contain duplicate combinations.
+Example
+Example 1:
+
+Input: candidates = [2, 3, 6, 7], target = 7
+Output: [[7], [2, 2, 3]]
+Example 2:
+
+Input: candidates = [1], target = 3
+Output: [[1, 1, 1]]
+    @param candidates: A list of integers
+    @param target: An integer
+    @return: A list of lists of integers
+    """
+    def combinationSum(self, candidates, target):
+        results = []
+        if not candidates:
+            return results
+        # deduplication
+        candidatesNew = sorted(list(set(candidates)))
+        self.dfs(candidatesNew, 0, [], target, results)
+        return results
+
+    def dfs(self, candidatesNew, index, current_result, remainingTarget, results):
+        if remainingTarget == 0:
+            return results.append(list(current_result))
+        for i in range(index, len(candidatesNew)):
+            if remainingTarget < candidatesNew[i]:
+                break
+            current_result.append((candidatesNew[i]))
+            self.dfs(candidatesNew, i, current_result, remainingTarget - candidatesNew[i], results)
+            current_result.pop()
