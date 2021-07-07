702f078e21f70e0e9406e2879afb4bc36deb8dc8
th2zz <tzhang329@gmail.com>
2021-07-07 18:42:59 +0800

    add files

--- memoized_search/required/word_break_iii/solution.py
+++ memoized_search/required/word_break_iii/solution.py
@@ -38,10 +38,11 @@ Partition DP
         if not s or not dict:
             return 0
         max_len, lower_dict = self.initialize(dict)
+        # word break by dfs
         return self.memo_search(s.lower(), 0, max_len, lower_dict, {})
 
     def initialize(self, dict):
-        max_len, lower_dict = 0, {}  # 把所有词转成小写 记录最长长度
+        max_len, lower_dict = 0, set()  # 把所有词转成小写 记录最长word长度
         for word in dict:
             max_len = max(max_len, len(word))
             lower_dict.add(word.lower())
@@ -50,15 +51,15 @@ Partition DP
     def memo_search(self, s, index, max_len, lower_dict, memo):
         if index == len(s):
             return 1
-        if index in memo:
+        if index in memo:  # s[index..] processed before, return result
             return memo[index]
-        result = 0
-        for end in range(index + 1, len(s) + 1):
-            if end - index > max_len:
-                break
-            word = s[index: end]
-            if word not in lower_dict:
-                continue
-            result += self.memo_search(s, end, max_len, lower_dict, memo)
-        memo[index] = result
-        return memo[index]
+        result = 0  # s[index..] 方案总数
+        for end in range(index + 1, len(s) + 1):  # enumerate word end exclusive
+            if end - index > max_len:  # pruning based on max len in all words
+                break  # end - index = [index, ..., end - 1] substring length
+            word = s[index: end]  # get s substring [index, ..., end - 1] as word
+            if word not in lower_dict:  # skip if it is not in dict
+                continue  # recurse on s[end..] add that to current result
+            result += self.memo_search(s, end, max_len, lower_dict, memo)  # 这个word end就是下个word开始 find sol at index end
+        memo[index] = result  # memo: # possible sentences formed from s[index..]
+        return memo[index]  # original problem solution: memo[0]
