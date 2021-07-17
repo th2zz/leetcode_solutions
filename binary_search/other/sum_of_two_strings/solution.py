eb4908dffce6616d9c0876bea84f0e7e0d46ad42
TINGHE ZHANG <zhangtinghe@sensorsdata.cn>
2021-07-17 15:40:24 +0800

    comments

---
+++ binary_search/other/sum_of_two_strings/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    """https://www.lintcode.com/problem/1343/?_from=collection&fromId=161
+    Description
+Given you two strings which are only contain digit character. You need to return a string spliced by the sum of the bits.
+
+A and B are strings which are composed of numbers
+Example
+Example1:
+Input:
+A = "99"
+B = "111"
+Output: "11010"
+Explanation: because 9 + 1 = 10, 9 + 1 = 10, 0 + 1 = 1,connect them，so answer is "11010"
+Example2:
+Input:
+A = "2"
+B = "321"
+Output: "323"
+Explanation: because 2 + 1 = 3, 2 + 0 = 2, 3 + 0 = 3, connect them，so answer is "323"
+Tags
+String
+Simulation
+Company
+Quora
+    @param A: a string
+    @param B: a string
+    @return: return the sum of two strings
+    """
+    def SumofTwoStrings(self, A, B):
+        pass
