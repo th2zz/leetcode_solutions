2ca75034ba0d03d8b53994b2411e93fcf8850e13
TINGHE ZHANG <zhangtinghe@sensorsdata.cn>
2021-07-17 13:31:29 +0800

    largest divisble subsets

--- dynamic_programming/important/largest_divisible_subsets/solution.py
+++ dynamic_programming/important/largest_divisible_subsets/solution.py
@@ -32,7 +32,44 @@ Longest Increasing Subsequence
 Medium
 
     @param nums: a set of distinct positive integers
-    @return: the largest subset
+    @return: the largest subset s.t. every pair of integers is divisible one way or the other
     """
+
     def largestDivisibleSubset(self, nums):
-        pass
+        if not nums:
+            return []
+        nums = sorted(nums)  # dp[i] size of largest divisible subset ending with i
+        dp, prev = {x: 1 for x in nums}, {x: -1 for x in nums}  # prev is used to restore path
+        last_num = nums[0]  # last number and largest number in subset
+        for x in nums:
+            for factor in self.get_factors(x):
+                if factor not in dp:
+                    continue
+                if dp[x] < dp[factor] + 1:  # factor < x;  dp[x] = max(dp[x's factor]) + 1; +1 = include x to subset
+                    dp[x] = dp[factor] + 1
+                    prev[x] = factor  # record path
+            if dp[x] > dp[last_num]:
+                last_num = x
+        return self.get_path(prev, last_num)
+
+    # O(sqrt(n)) get all factors of x, excluding x itself
+    def get_factors(self, x):
+        if x == 1:
+            return []
+        factor = 1
+        factors = []
+        while factor * factor <= x:  # 分界线是 sqrt(x) ; 只需要遍历一半
+            if x % factor == 0:
+                factors.append(factor)  # 放上这个因子
+                if factor * factor != x and factor != 1:  # 不重复 不为1
+                    factors.append(x // factor)  # 放上另一半
+            factor += 1
+        return factor
+
+    # restore path with prev dict start from last_num
+    def get_path(self, prev, last_num):
+        path = []
+        while last_num != -1:
+            path.insert(0, last_num)
+            last_num = prev[last_num]
+        return path
